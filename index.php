7d7f136b4f9cc83a04cc20b3f48d14d8ec50867c
amefad <fame@libero.it>
2020-04-22 19:09:35 +0200

    Add js libraries

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php
-
+<html>
+    <head>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+  integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+  crossorigin=""/>
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+  integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+  crossorigin=""></script>
+   <script src="./js/mappa.js"></script>
+    </head>
+    <body>
 <?php
 if(isset($_POST['submit'])){
 
@@ -46,6 +55,6 @@ if(isset($error)){
 <p><label>Email</label><br><input type='text' name='email' value=''></p> 
 <p><input type='submit' name='submit' value='Submit'></p> 
 </form>
+    </body>
+    </html>
 
-
-?>
